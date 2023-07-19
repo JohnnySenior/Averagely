43c2fc925006373f7427190bc2505b91431dd891
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-19 12:27:58 +0900

    Add-Documentation

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
-﻿Console.WriteLine("You can calculate you average grade with this app");
+﻿Console.WriteLine("You can calculate your average grade with this app");
 Console.WriteLine("Enter all grades per subject below");
 Console.WriteLine("Note: You should enter [0 ~ 30]");
 Console.WriteLine();
 
 Console.Write("Computer Science: ");
-var comScience = double.Parse(Console.ReadLine()); 
+double comScience = double.Parse(Console.ReadLine()); 
 
 Console.Write("English: ");
-var english = double.Parse(Console.ReadLine()); 
+double english = double.Parse(Console.ReadLine()); 
 
 Console.Write("Mathematics: ");
-var math = double.Parse(Console.ReadLine()); 
+double math = double.Parse(Console.ReadLine()); 
 
 Console.Write(".Net Programming: ");
-var net = double.Parse(Console.ReadLine()); 
+double net = double.Parse(Console.ReadLine()); 
 
 Console.Write("Data Structure: ");
-var structure = double.Parse(Console.ReadLine()); 
+double structure = double.Parse(Console.ReadLine()); 
 
-var average = (comScience + english + math + net + structure)/5;
+double average = (comScience + english + math + net + structure)/5;
 Console.WriteLine($"Average: {average}");
 Console.WriteLine();
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Averagely
-Usefull app for student who calculate average grade of subjects
+
+Usefull app for student who calculate average grade of subjects.
+
+I have used basic functions and arithmetic operations:
+
+* Console input/output
+
+    ![Alt text](image.png)
+
+* Arithmetic operation
+
+    ![Alt text](image-1.png)
+
+If you want to run my code in your local computer, you can clone my code.
+
+Click here for instruction **[video](https://www.loom.com/share/9cebefe04b8b40f0bb6c48f87f58ad59?sid=e90b647e-a46f-46e2-bca3-9f89aa2512f5)**
