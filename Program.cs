9ac0deb0942ce7d2f7b3c4fd45d28ab43b2392a3
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-29 21:38:55 +0900

    Add-Exception-Part

--- Program.cs
+++ Program.cs
@@ -4,23 +4,41 @@ Console.WriteLine("Enter all grades per subject below");
 Console.WriteLine("Note: You should enter [1 ~ 5]");
 Console.Write("How many subject do you want to enter: ");
 
-int numberOfSubject = int.Parse(Console.ReadLine());
-Console.Clear();
+int numberOfSubject = 0;
+
+try
+{
+    numberOfSubject = int.Parse(Console.ReadLine());
+    Console.Clear();
+}
+catch (FormatException formatException)
+{
+    Console.WriteLine("The input is not in a correct format");
+}
+catch (OverflowException overflowException)
+{
+    Console.WriteLine("Value is too large or too small, take a look data type.");
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
+
 double totalGrade = 0;
 
-for(int iteration = 1; iteration <= numberOfSubject; iteration++)
+for (int iteration = 1; iteration <= numberOfSubject; iteration++)
 {
     Console.Write($"Subject {iteration}: ");
-    double grade = int.Parse(Console.ReadLine());
+    double grade = double.Parse(Console.ReadLine());
     totalGrade += grade;
 }
 
 double average = totalGrade / numberOfSubject;
-Console.WriteLine($"Average grade: {average}");
+Console.WriteLine($"Average grade: {average:f2}");
 
-if(average >= 3 && average <= 5)
+if (average >= 3 && average <= 5)
     Console.WriteLine("You are passed");
-else if(average < 3 && average > 0)
+else if (average < 3 && average > 0)
     Console.WriteLine("You are failed");
 else
     Console.WriteLine("You entered wrong grade, please check again");
